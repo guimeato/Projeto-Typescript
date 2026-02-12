42502634db1a51a9ca15b8833f50f7155c5a8800
Guilherme Meato <guimeato@gmail.com>
2026-02-11 23:07:02 -0300

    refactor: add nova estrutura no ex 3

--- src/ex3.ts
+++ src/ex3.ts
@@ -1,15 +1,17 @@
-interface ILivro {
+interface Livro {
   titulo: string,
   autor: string,
   anoPublicacao: number
 }
 
-const resumirLivro = (livro:ILivro) => {
-  console.log(`O livro '${livro.titulo}' foi escrito por ${livro.autor} em ${livro.anoPublicacao}.`);
+function resumirLivro(livro:Livro) {
+  return(`O livro '${livro.titulo}' foi escrito por ${livro.autor} em ${livro.anoPublicacao}.`);
 }
 
-resumirLivro({
+const livroExemplo:Livro = ({
   titulo: "Dom Casmurro",
   autor: "Machado de Assis",
   anoPublicacao: 1899
 });
+
+console.log(resumirLivro(livroExemplo));
